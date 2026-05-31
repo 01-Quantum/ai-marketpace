ccd3e6fd777bbe72f1e4562de8c8d7a12cbcc2d4
Alex Shpurov <alextmn1@gmail.com>
2026-05-31 11:50:56 -0400

    Add environment configuration for Supabase integration. Introduced environment files for production and development, including necessary Supabase credentials. Updated angular.json to handle file replacements for different environments. Added Supabase JS SDK to package dependencies.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.types';
+
+export const environment: Environment = {
+  production: true,
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.types';
+
+export const environment: Environment = {
+  production: false,
+  supabaseUrl: 'https://hzydimksdmsrzrmqwayz.supabase.co',
+  supabaseAnonKey: 'sb_publishable_FlUQEkjEyein8dH4-zGhMw_PkC5n7SR',
+};
